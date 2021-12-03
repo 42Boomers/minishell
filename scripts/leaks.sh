871ea14f46fb5600279ca52894e18cf0bf179dfc
tglory <tglory@student.42Lyon.fr>
2021-12-03 06:09:37 +0100

    add leaks to test

--- scripts/leaks.sh
+++ scripts/leaks.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	make all > /dev/null && valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose ./minishell $@
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+if [[ "$OSTYPE" == "darwin"* ]]; then
 	make all > /dev/null && leaks -atExit -- ./minishell $@
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	make all > /dev/null && valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./minishell $@
 else
 	echo "Unknown OS $OSTYPE"
+	make all > /dev/null && valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./minishell $@
 fi
