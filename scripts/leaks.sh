9ab285951c3fff946ffb3e4f44e34f3a75b50b50
tglory <tglory@student.42Lyon.fr>
2021-12-03 07:32:41 +0100

    up

--- scripts/leaks.sh
+++ scripts/leaks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [ "$OSTYPE" == "darwin"* ]; then
 	make all > /dev/null && leaks -atExit -- ./minishell $@
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+elif [ "$OSTYPE" == "linux-gnu"* ]; then
 	make all > /dev/null && valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./minishell $@
 else
 	echo "Unknown OS $OSTYPE"
