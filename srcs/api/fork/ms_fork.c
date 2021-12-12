fe03080f8d77bda58a70329df0dd1a2aa08217d1
Maximilien Rozniecki <blueyesmax69@gmail.com>
2021-12-12 13:13:48 +0100

    pipe and fork working fine

--- srcs/api/fork/ms_fork.c
+++ srcs/api/fork/ms_fork.c
@@ -6,7 +6,7 @@
 /*   By: tglory <tglory@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 10:07:29 by tglory            #+#    #+#             */
-/*   Updated: 2021/12/05 16:47:52 by tglory           ###   ########lyon.fr   */
+/*   Updated: 2021/12/12 13:01:49 by mrozniec         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,85 +60,17 @@ static void	ms_child(t_master *master, char *command, char **args)
 		printf("\e[31mminishell: %s: command not found\n\e[0m", command);
 	}
 }
-/*
-** version 1.0 working with one pipe
-** need some modification to work with more pipe
-*/
 
-/*
-void	ms_fork2(t_master *master, char *command, char **args, int pipe_check)
-{
-	int		pip_end[2];
-	int		*status;
-	int		pip_rec;
-	pid_t	fork_id;
-	pid_t	pip_fork_id;
-
-	//printf("i was here %d", pipe_check);
-	if (pipe_check > 0)
-	{
-		if (pipe(pip_end) == -1)
-		{
-			ft_println_red("Error > An error has occured while pipe creation");
-			return ;
-		}
-	}
-	fork_id = fork();
-	if (fork_id < 0)
-	{
-		ft_println_red("Error > An error has occured while fork creation");
-		return ;
-	}
-	if (fork_id == 0)
-	{
-		if (pipe_check > 0)
-		{
-			close(pip_end[0]);
-			dup2(pip_end[1], 1);
-		}
-		ms_child(master, command, args);
-	}
-	else
-	{
-		if (pipe_check > 0)
-		{
-			pip_fork_id = fork();
-			if (pip_fork_id < 0)
-			{
-				ft_println_red("Error > An error has occured while fork creation");
-				return ;
-			}
-			if (pip_fork_id == 0)
-			{
-				close(pip_end[1]);
-				dup2(pip_end[0], 0);
-				pip_rec = ft_pipe_check(&args[pipe_check]);
-				if (pip_rec > 0)
-					ms_fork2(master, args[pipe_check], &args[pipe_check + 1], \
-					pip_rec);
-				else if (pip_rec == 0)
-					ms_child(master, args[pipe_check], &args[pipe_check + 1]);
-			}
-		}
-	}
-	status = NULL;
-	if (pipe_check > 0)
-	{
-		close(pip_end[0]);
-		close(pip_end[1]);
-		waitpid(pip_fork_id, status, 0);
-	}
-	waitpid(fork_id, status, 0);
-}*/
 void	ms_fork2(t_master *master, char *command, char **args)
 {
 	int		pip_end[2];
+	int		fd_in;
 	int		*status;
 	int		pip_rec;
 	pid_t	fork_id;
 
-	pip_rec = 5;
-	//printf("i was here %d", pipe_check);
+	fd_in = 0;
+	pip_rec = 1;
 	while (pip_rec > 0)
 	{
 		if (pipe(pip_end) == -1)
@@ -146,51 +78,33 @@ void	ms_fork2(t_master *master, char *command, char **args)
 			ft_println_red("Error > An error has occured while pipe creation");
 			return ;
 		}
-	}
-	fork_id = fork();
-	if (fork_id < 0)
-	{
-		ft_println_red("Error > An error has occured while fork creation");
-		return ;
-	}
-	if (fork_id == 0)
-	{
-		if (pipe_check > 0)
+		fork_id = fork();
+		if (fork_id < 0)
 		{
-			close(pip_end[0]);
-			dup2(pip_end[1], 1);
+			ft_println_red("Error > An error has occured while fork creation");
+			return ;
 		}
-		ms_child(master, command, args);
-	}
-	else
-	{
-		if (pipe_check > 0)
+		if (fork_id == 0)
 		{
-			pip_fork_id = fork();
-			if (pip_fork_id < 0)
-			{
-				ft_println_red("Error > An error has occured while fork creation");
-				return ;
-			}
-			if (pip_fork_id == 0)
+			dup2(fd_in, 0);
+			pip_rec = ft_pipe_check(args);
+			if (pip_rec > 0)
+				dup2(pip_end[1], 1);
+			close(pip_end[0]);
+			ms_child(master, command, args);
+		}
+		else
+		{
+			status = NULL;
+			waitpid(fork_id, status, 0);
+			pip_rec = ft_pipe_check(args);
+			if (pip_rec > 0)
 			{
 				close(pip_end[1]);
-				dup2(pip_end[0], 0);
-				pip_rec = ft_pipe_check(&args[pipe_check]);
-				if (pip_rec > 0)
-					ms_fork2(master, args[pipe_check], &args[pipe_check + 1], \
-					pip_rec);
-				else if (pip_rec == 0)
-					ms_child(master, args[pipe_check], &args[pipe_check + 1]);
+				fd_in = pip_end[0];
+				command = args[pip_rec];
+				args = &args[pip_rec + 1];
 			}
 		}
 	}
-	status = NULL;
-	if (pipe_check > 0)
-	{
-		close(pip_end[0]);
-		close(pip_end[1]);
-		waitpid(pip_fork_id, status, 0);
-	}
-	waitpid(fork_id, status, 0);
 }
