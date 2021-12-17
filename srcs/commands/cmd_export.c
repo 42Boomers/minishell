cc5e2904890aa335d54a9b00a0e4314d762628a9
mac <mac@mac-pro-de-mac.home>
2021-12-17 14:29:59 +0100

    patch export avec key invalide il reste export sans arg, sans '='

--- srcs/commands/cmd_export.c
+++ srcs/commands/cmd_export.c
@@ -18,15 +18,42 @@ static t_bool	ms_export_analyze(t_ms_input *input)
 	return (TRUE);
 }
 
+int    check_export_arg(char *arg)
+{
+    int        i;
+
+    if (!ft_isalpha(arg[0]) && arg[0] != '_')
+        return (FALSE);
+    i = 0;
+    while (arg[i])
+    {
+        if (!ft_isalnum(arg[i]) && arg[i] != '_')
+            return (FALSE);
+        i++;
+    }
+    return (TRUE);
+}
+
 static t_bool	ms_export_execute(t_ms_input *input)
 {
 	int	i;
+	t_env *tmp;
 
 	input->cmd->master->cmd_ret = 0;
 	i = 0;
 	while (input->args_size > i)
 	{
-		if (!ms_env_add_raw(input->cmd->master, input->args[i]))
+		tmp = ms_env_create(input->args[i]);
+		if (!check_export_arg(tmp->key))
+		{
+			ft_putstr_fd("minishell: export: '", 2);
+			if (*tmp->key)
+				ft_putstr_fd(tmp->key, 2);
+			else
+				ft_putstr_fd(input->args[i], 2);
+			ft_putendl_fd("': not a valid identifier", 2);
+		}
+		else if (!ms_env_add_raw(input->cmd->master, input->args[i]))
 			printf("Can't add %s\n", input->args[i]);
 		i++;
 	}
