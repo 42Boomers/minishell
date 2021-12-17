489f364fb9758855797330b5ffb58d4a6fba3f40
Maximilien Rozniecki <blueyesmax69@gmail.com>
2021-12-17 14:59:03 +0100

    reprduct reaction with multiple redirect in the same direction in one cmd done

--- srcs/api/redirect/redirect.c
+++ srcs/api/redirect/redirect.c
@@ -44,22 +44,28 @@ static void	ms_red_in(char **args, int *fd_red_in)
 	int		pos;
 
 	pos = ft_red_in_check(args);
-	if (pos > 0)
-	{
-		*fd_red_in = open(args[pos], O_RDONLY);
-		ms_del_red(args, pos);
-	}
-	else if (pos < 0)
-	{
-		pos = -pos;
-		*fd_red_in = open(".ms_herefile", O_CREAT | O_WRONLY | O_TRUNC, 0641);
-		ms_heredoc(*fd_red_in, args[pos]);
-		*fd_red_in = open(".ms_herefile", O_RDONLY);
-		unlink(".ms_herefile");
-		ms_del_red(args, pos);
-	}
-	else
+	if (pos == 0)
 		fd_red_in[0] = 0;
+	while (pos != 0)
+	{
+		if (pos > 0)
+		{
+			*fd_red_in = open(args[pos], O_RDONLY);
+			ms_del_red(args, pos);
+		}
+		else if (pos < 0)
+		{
+			*fd_red_in = open(".ms_herefile", O_CREAT | O_WRONLY | O_TRUNC, \
+			0641);
+			ms_heredoc(*fd_red_in, args[-pos]);
+			*fd_red_in = open(".ms_herefile", O_RDONLY);
+			unlink(".ms_herefile");
+			ms_del_red(args, -pos);
+		}
+		pos = ft_red_in_check(args);
+		if (pos != 0)
+			close(*fd_red_in);
+	}
 }
 
 static int	ft_red_out_check(char **args)
@@ -94,19 +100,26 @@ static void	ms_red_out(char **args, int *fd_red_out)
 	int	pos;
 
 	pos = ft_red_out_check(args);
-	if (pos > 0)
-	{
-		fd_red_out[1] = open(args[pos], O_CREAT | O_WRONLY | O_TRUNC, 0641);
-		ms_del_red(args, pos);
-	}
-	else if (pos < 0)
-	{
-		pos = -pos;
-		fd_red_out[1] = open(args[pos], O_CREAT | O_WRONLY | O_APPEND, 0641);
-		ms_del_red(args, pos);
-	}
-	else
+	if (pos == 0)
 		fd_red_out[1] = 0;
+	while (pos != 0)
+	{
+		if (pos > 0)
+		{
+			fd_red_out[1] = open(args[pos], O_CREAT | O_WRONLY | O_TRUNC, 0641);
+			ms_del_red(args, pos);
+		}
+		else if (pos < 0)
+		{
+			pos = -pos;
+			fd_red_out[1] = open(args[pos], O_CREAT | O_WRONLY | O_APPEND, \
+			0641);
+			ms_del_red(args, pos);
+		}
+		pos = ft_red_out_check(args);
+		if (pos != 0)
+			close(fd_red_out[1]);
+	}
 }
 
 void	ms_red_in_out(char **args, int *redir)
