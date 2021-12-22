16885974b42a29068b464eaede2eb03a8ba828e0
Sylvain <sylducam@gmail.com>
2021-12-22 16:52:56 +0100

    let's hope it works

--- srcs/signals/handler_signal.c
+++ srcs/signals/handler_signal.c
@@ -11,10 +11,69 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+
 /*------------------------------------------------------------------------------
 version without segfault
 ------------------------------------------------------------------------------*/
 
+// static void	ctrl_c_fork(void)
+// {
+// }
+// static void	ctrl_c_normal(void)
+// {
+// }
+
+static void	ctrl_c_fork(void)
+{
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	g_ctrl_c = 0;
+}
+
+static void	ctrl_c_normal(void)
+{
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+static void	handle_signal(int signum, siginfo_t *sig_info, void *ucontext_t)
+{
+	(void)sig_info;
+	(void)ucontext_t;
+	if (signum == SIGINT)
+	{
+		if (g_ctrl_c == 0)
+			ctrl_c_normal();
+		if (g_ctrl_c == 1)
+			ctrl_c_fork();
+	}
+}
+
+static int	register_handler(int signum)
+{
+	struct sigaction	sig;
+
+	sig.sa_flags = SA_SIGINFO;
+	sig.sa_sigaction = handle_signal;
+	return (sigaction(signum, &sig, NULL));
+}
+
+void	ms_register_signals(t_master *master)
+{
+	int	fd;								//
+										//
+	fd = open(".ms_heredoc", O_RDONLY); // syl:toute cette partie sur ms_heredoc
+	if (fd != -1)						// est a revoir, Max dit qu'elle n'est
+	{									// pas bonne. Voir avec lui.
+		unlink(".ms_heredoc");			//
+		close(fd);						//
+	}
+	register_handler(SIGINT);
+	(void)master;
+}
 
 
 /*------------------------------------------------------------------------------
